5e6b988eaccc60e56c3f24103d1c4ef7eafaceb5
Oscar <oscarsantana2014@gmail.com>
2021-11-26 17:07:50 -0600

    Solved Barking dog challenge

---
+++ src/BarkingDog.java
@@ -0,0 +1,32 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking,int hourOfDay){
+//if (barking && hourOfDay < 8 || hourOfDay > 22){
+//    return true;
+//}else if (hourOfDay > 0 || hourOfDay < 23){
+//    return false;
+//}else{
+//    return false;
+//}
+
+if(hourOfDay < 0 || hourOfDay > 23){
+    return false;
+}else return barking && hourOfDay < 8 || hourOfDay > 22;
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println( shouldWakeUp(true,1));
+        System.out.println( shouldWakeUp(false,2));
+        System.out.println( shouldWakeUp(true,8));
+        System.out.println( shouldWakeUp(true,-1));
+    }
+
+}
+//hourOfDay has a valid range of 0-23
+//If dog barks before 8 or after 22 return true;
+//else false
+//if hourOfDay is less than 0 or greater than 23 return false
