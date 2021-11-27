f92de8106ce6bc4fbbb25510be2961cc2e1de04d
Oscar <oscarsantana2014@gmail.com>
2021-11-26 22:03:16 -0600

    Solution to decimal comparator

---
+++ src/DecimalOperator.java
@@ -0,0 +1,19 @@
+public class DecimalOperator {
+    public static boolean areEqualByThreeDecimalPlaces(double myFirstDouble, double mySecondDouble){
+        int myFirstCheck = (int)(myFirstDouble * 1000);
+        int mySecondCheck = (int)(mySecondDouble * 1000);
+
+        if(myFirstCheck - mySecondCheck == 0){
+
+            return true;
+        } else
+
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756,-3.175));
+    }
+
+}
