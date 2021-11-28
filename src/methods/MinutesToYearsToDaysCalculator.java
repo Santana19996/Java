3794f1def35c98347e7f3c62a02ab83aaf577799
Oscar <oscarsantana2014@gmail.com>
2021-11-27 22:33:29 -0600

    MinutesToYearsToDaysCalculator challenge complete

---
+++ src/methods/MinutesToYearsToDaysCalculator.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class MinutesToYearsToDaysCalculator {
+
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        }else{
+            long days= (minutes % 525600) /1440 ;
+            long years = minutes / 525600;
+            System.out.println(years + " y" + " and " + days + " d");
+        }
+    }
+
+
+    public static void main(String[] args) {
+printYearsAndDays(525600);
+printYearsAndDays(1051200);
+printYearsAndDays(561600);
+    }
+}
+//1 hour = 60 minutes
+//1 day = 24 hours
+//1 year = 365 days
