71c1e78c66630eda7daf8aa0cbc944990af00688
Oscar <oscarsantana2014@gmail.com>
2021-11-30 20:58:22 -0600

    DayOfTheWeeKChallenge Solution

--- src/Hello.java
+++ src/Hello.java
@@ -8,5 +8,21 @@ public class Hello {
         int myTotal = myFirstNumber + mySecondNumber +  myThirdNumber * -100;
         System.out.println(myTotal);
 
+
+
+//        Switch Case Practice
+
+        char charValue = 'b';
+
+        switch (charValue){
+           case 'a':
+               System.out.println("a was found");
+               break;
+               case 'b':
+                   System.out.println("b was found");
+            case 'c':case 'd':case 'e':
+                System.out.println("could not fund a,b,c,d,e");
+        }
+
     }
 }
