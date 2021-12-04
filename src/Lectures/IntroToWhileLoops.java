b636a1612837b3718fcd6a4fa0c0d7e33e4d32b9
Oscar <oscarsantana2014@gmail.com>
2021-12-03 18:30:47 -0600

    End of while lectuure

--- src/Lectures/IntroToWhileLoops.java
+++ src/Lectures/IntroToWhileLoops.java
@@ -1,6 +1,12 @@
 package Lectures;
 
+import methods.IsEvenNumber;
+
 public class IntroToWhileLoops {
+
+    public static boolean isEvenNumber(int number){
+        return number % 2 ==0;
+    }
     public static void main(String[] args) {
         int count =6;
         while(count !=6){
@@ -15,5 +21,14 @@ public class IntroToWhileLoops {
             System.out.println("Count value was " + count);
             count++;
         }while (count !=6);
+        int number = 4;
+        int finishNumber =20;
+         while (number <=finishNumber){
+             number++;
+             if (!isEvenNumber(number)){
+                 continue;
+             }
+             System.out.println("Even number " + number);
+         }
     }
 }
