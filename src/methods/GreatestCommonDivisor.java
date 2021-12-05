1f908007215fb0073d2c1d81c2c4d8df91c08c81
Oscar <oscarsantana2014@gmail.com>
2021-12-05 13:54:33 -0600

    Solution to Greatest Common Divisor

---
+++ src/methods/GreatestCommonDivisor.java
@@ -0,0 +1,22 @@
+package methods;
+
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+        int gcd = 0;
+        for (int i = 1; i <= first && i <= second; i++) {
+            if (first % i == 0 && second % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25,15));
+        System.out.println(getGreatestCommonDivisor(12,30));
+        System.out.println(getGreatestCommonDivisor(9,18));
+    }
+}
