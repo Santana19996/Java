995bf72f1d672cc315bbf8f835d15d336b58c07e
Oscar <oscarsantana2014@gmail.com>
2021-12-09 17:55:07 -0600

    end of scanner lecture

--- src/Lectures/ReadingUserInput.java
+++ src/Lectures/ReadingUserInput.java
@@ -1,2 +1,31 @@
-package Lectures;public class ReadingUserInput {
+package Lectures;
+
+import java.util.Scanner;
+
+public class ReadingUserInput {
+    public static void main(String[] args) {
+        Scanner readInput = new Scanner(System.in);
+
+        System.out.println("Enter your Year of birth ");
+        boolean hasNextInt = readInput.hasNextInt();
+        if (hasNextInt) {
+
+            int dob = readInput.nextInt();
+            readInput.nextLine();
+
+            System.out.println("Enter your name: ");
+            String name = readInput.nextLine();
+            int age = 2021 - dob;
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and your are " + age + " years old ");
+            } else {
+                System.out.println("You are not born yet");
+            }
+        }else {
+            System.out.println("You have entered a wrong input");
+        }
+
+        readInput.close();
+    }
+
 }
