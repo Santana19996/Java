57c5f681fad54c0248c7cd39d4a4b3d04f41621a
Oscar <oscarsantana2014@gmail.com>
2021-12-10 12:54:32 -0600

    Solution to input calculator

---
+++ src/methods/InputCalculator.java
@@ -0,0 +1,23 @@
+package methods;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner readInput = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+
+        while (readInput.hasNextInt()) {
+            sum += readInput.nextInt();
+            count++;
+        }
+        System.out.println("Sum: " + sum + "Average: " + sum / count);
+        readInput.close();
+    }
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+}
+
