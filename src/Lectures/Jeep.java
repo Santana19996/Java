ac9935e4b45a84281d3045da2a8912b4c1a3d5e9
Oscar <oscarsantana2014@gmail.com>
2022-01-03 12:37:00 -0600

    New class test

---
+++ src/Lectures/Jeep.java
@@ -0,0 +1,12 @@
+package Lectures;
+
+public class Jeep extends Car {
+    private String model;
+
+    public Jeep(String name, String size, int wheels, int doors, int gears, boolean isManual) {
+        super(name, size, wheels, doors, gears, isManual);
+        this.model = "Grand Cherokee";
+    }
+
+
+}
--- src/Lectures/Main.java
+++ src/Lectures/Main.java
@@ -3,9 +3,15 @@ package Lectures;
 public class Main {
 
     public static void main(String[] args) {
-        Outlander outlander = new Outlander(36);
-        outlander.steer(45);
-        outlander.accelerate(30);
-        outlander.accelerate(20);
+//        Outlander outlander = new Outlander(36);
+//        outlander.steer(45);
+//        outlander.accelerate(30);
+//        outlander.accelerate(20);
+
+        Jeep jeep = new Jeep("Grand Cherokee","Large",4,4,8,false);
+jeep.steer(45);
+
+
+
     }
 }
