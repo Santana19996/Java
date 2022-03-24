87d1194dcace0887c64fb6575cc9be130b5755d7
Oscar <oscarsantana2014@gmail.com>
2022-03-23 20:34:54 -0500

    Starting hangman project

---
+++ src/methodsAndChallenges/HangmanGame.java
@@ -0,0 +1,22 @@
+package methodsAndChallenges;
+
+import java.util.Scanner;
+
+public class HangmanGame {
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Oscar's Hangman Game");
+        System.out.println("*********************************");
+        String word = "Hangman";
+
+
+
+    }
+}
