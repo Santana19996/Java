95a5e55f8b8394cdcbbe6e8277ac8bbf8a7c3ec9
oscar.santana <oscar.santana@cgi.com>
2022-04-25 10:41:43 -0500

    update

---
+++ src/methodsAndChallenges/SimpleAudioPlayer.java
@@ -0,0 +1,52 @@
+package methodsAndChallenges;
+
+
+import javax.print.attribute.standard.Media;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SimpleAudioPlayer {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("data/90sflav.wav");
+
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+
+        Clip clip = AudioSystem.getClip();
+
+        clip.open(audioInputStream);
+
+        String response = "";
+
+        while (!response.equals("Q")) {
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.print("Enter your choice: ");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"):
+                    clip.start();
+                    break;
+                case ("S"):
+                    clip.stop();
+                    break;
+                case ("R"):
+                    clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"):
+                    clip.close();
+                    break;
+                default:
+                    System.out.println("Not a valid response");
+            }
+        }
+        System.out.println("Byeeee!");
+    }
+}
--- src/methodsAndChallenges/StringToBinary.java
+++ src/methodsAndChallenges/StringToBinary.java
@@ -3,4 +3,12 @@ package methodsAndChallenges;
 public class StringToBinary {
 
 
+
+    public static void main(String[] args) {
+        String input = "Hello";
+
+
+
+    }
+
 }
