482f7f1325c4ca6ff10c2fab1c14c8cb5b65b5e0
oscar.santana <oscar.santana@cgi.com>
2022-06-14 22:20:05 -0500

    end of night

---
+++ src/methodsAndChallenges/LoopTask7.java
@@ -0,0 +1,16 @@
+package methodsAndChallenges;
+
+public class LoopTask7 {
+    public static void main(String[] args) {
+        Boolean isEven;
+
+        for (int i = 0; i <= 19; i++) {
+if (i %2==0){
+    System.out.println(i + " - Is even");
+}
+else{
+    System.out.println(i + " - Is odd");
+}
+        }
+    }
+}
