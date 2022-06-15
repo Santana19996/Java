1a538e7519e9a4ef124935f828b305ed383fb476
oscar.santana <oscar.santana@cgi.com>
2022-06-15 10:03:00 -0500

    add while loop prac

--- src/methodsAndChallenges/LoopTask10.java
+++ src/methodsAndChallenges/LoopTask10.java
@@ -1,2 +1,36 @@
-package methodsAndChallenges;public class LoopTask10 {
+package methodsAndChallenges;
+
+import java.util.Scanner;
+
+public class LoopTask10 {
+    public static void main(String[] args) {
+        String username = "oscar";
+        String password = "java";
+
+
+        System.out.println("Welcome to Javagram sign in below!");
+        System.out.println(" ");
+
+        System.out.print("Username:");
+        Scanner readInput = new Scanner(System.in);
+        String usernameInput = readInput.nextLine();
+        System.out.print("Password:");
+        String userPassword = readInput.nextLine();
+
+
+        while (!usernameInput.equals(username) && !userPassword.equals(password)){
+            System.out.print("Incorrect Please Try again: " );
+            System.out.println(" ");
+            System.out.print("Username: ");
+            usernameInput=readInput.nextLine();
+
+            System.out.print("Password: ");
+            userPassword=readInput.nextLine();
+
+        }
+        System.out.println("Sign in Successful Welcome!");
+    }
+
+
+
 }
--- src/methodsAndChallenges/LoopTask9.java
+++ src/methodsAndChallenges/LoopTask9.java
@@ -1,2 +1,21 @@
-package methodsAndChallenges;public class LoopTask9 {
+package methodsAndChallenges;
+
+import java.util.Scanner;
+
+public class LoopTask9 {
+
+    public static void main(String[] args) {
+        int secretNumber = (int) (Math.random() * 5 + 1);
+
+
+        System.out.print("I chose a number between 1 and 5. Try to guess it : ");
+        Scanner readInput = new Scanner(System.in);
+        int userGuess = readInput.nextInt();
+
+        while (userGuess != secretNumber) {
+            System.out.print("Guess again: ");
+            userGuess = readInput.nextInt();
+        }
+        System.out.println("You got it!");
+    }
 }
