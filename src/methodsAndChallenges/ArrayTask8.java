3177502a793caed9db7cac89fdbc21f91186b520
oscar.santana <oscar.santana@cgi.com>
2022-06-16 13:33:34 -0500

    done

--- src/methodsAndChallenges/ArrayTask8.java
+++ src/methodsAndChallenges/ArrayTask8.java
@@ -1,2 +1,18 @@
-package methodsAndChallenges;public class ArrayTask8 {
+package methodsAndChallenges;
+
+import java.util.Arrays;
+
+public class ArrayTask8 {
+    public static void main(String[] args) {
+        double[] prices= {1.99,2.99,3.99,4.99};
+        double[]newPrices= new double[prices.length];
+
+
+        for (int i = 0; i <prices.length ; i++) {
+        newPrices[i]= prices[i] * 1.13;
+        }
+
+        System.out.println("The original prices are: " + Arrays.toString(prices));
+        System.out.println("The after tax prices are: " + Arrays.toString(newPrices));
+    }
 }
--- src/methodsAndChallenges/ArrayTask9.java
+++ src/methodsAndChallenges/ArrayTask9.java
@@ -1,2 +1,30 @@
-package methodsAndChallenges;public class ArrayTask9 {
+package methodsAndChallenges;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayTask9 {
+    public static void main(String[] args) {
+        System.out.println("How many pizza toppings do you want?.");
+        Scanner readInput = new Scanner(System.in);
+        int userAmount = readInput.nextInt();
+        String[] toppings = new String[userAmount];
+
+        System.out.println("Great , enter each topping!");
+        for (int i = 0; i <toppings.length ; i++) {
+            System.out.print(i + ". ");
+            toppings[i] = readInput.next();
+
+        }
+        System.out.println("Thank you Here are the toppings you Ordered!");
+
+        for (int i = 0; i <toppings.length ; i++) {
+            System.out.println( i + ". " +  toppings[i]);
+        }
+        System.out.println("Press anything to confirm your order.");
+        readInput.next();
+        System.out.println("Great, a driver is on the way!");
+    }
+
+
 }
